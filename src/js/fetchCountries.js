7ebad160db999cb35ee318310f41dd37957c932f
vokur13 <vokur13@gmail.com>
2022-09-06 07:52:36 +0300

    throw new Error(response.status) added

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,38 +1,39 @@
 const BASE_URL = 'https://restcountries.com/v3.1';
 
 const searchParams = new URLSearchParams({
-        fields:['name','capital','population','flags','languages']
-      });
+  fields: ['name', 'capital', 'population', 'flags', 'languages'],
+});
 
-      export const fetchCountries = (name) => {
-        return fetch(`${BASE_URL}/name/${name}?${searchParams}`).then((response) => {
-          return response.json();
-        });
-      };
+export const fetchCountries = name => {
+  return fetch(`${BASE_URL}/name/${name}?${searchParams}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
 
-//       
+//
 
-      
 //       refs.input.addEventListener('input', _.debounce(onFilterLook, 250));
-      
+
 //       const listItemsMarkup = itemsMap(tech);
 //       populateList(listItemsMarkup);
-      
+
 //       function itemsMap(items) {
 //         return items.map(item => `<li>${item.label}</li>`).join('');
 //       }
-      
+
 //       function onFilterLook(e) {
 //         console.log('INPUT');
 //         const filter = e.target.value.toLowerCase();
-      
+
 //         const filteredItems = tech.filter(item => item.label.toLowerCase().includes(filter));
-      
+
 //         const listItemsMarkup = itemsMap(filteredItems);
 //         populateList(listItemsMarkup);
 //       }
-      
+
 //       function populateList(markup) {
 //         refs.list.innerHTML = markup;
 //       }
-      
